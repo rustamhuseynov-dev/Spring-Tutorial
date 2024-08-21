cc00c94d3ae7470d81ebe02077de2644fa59e523
rustemcoding571 <rustem.huseynov.2016@gmail.com>
2024-08-21 15:27:50 +0400

    fieldlarin uzerine @Value annotasiyasi ile yaml fayldan data gondermek

--- src/main/java/az/rustam/microservicebootcamp/rest/SampleController.java
+++ src/main/java/az/rustam/microservicebootcamp/rest/SampleController.java
@@ -5,10 +5,13 @@ import az.rustam.microservicebootcamp.service.TestService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/test")
@@ -25,10 +28,22 @@ public class SampleController {
 //        this.testService = testService;
 //    }
 
+    @Value("${rustam.bootcamp.name}")
+    private String name;
+
+    @Value("#{'${course.students}'}")
+    private List<String> students;
+
+    @Value("#{new Integer('${course.count}')}")
+    private Integer count;
+
 
     @GetMapping(path = "/{id}")
     public String test(@PathVariable Long id){
         log.info(studentServiceImpl.toString());
+        log.info("application-yaml-name {} ",name);
+        log.info("course students {}",students);
+        log.info("count {}",count);
         return "Hello";
     }
 }
